86048b4ee417246aa530c65e8a4cda8b5a8431a9
yujie-chen119 <blairyujie@gmail.com>
2026-08-02 06:58:15 +0800

    Update CureSystem.java

--- src/contagion/CureSystem.java
+++ src/contagion/CureSystem.java
@@ -1,5 +1,44 @@
 package contagion;
 
+/**
+ * Keeps track of the cure progress.
+ *
+ * @author YU-JIE CHEN
+ */
 public class CureSystem {
 
+    private double progress;
+
+    public CureSystem() {
+        progress = 0;
+    }
+
+    public double getProgress() {
+        return progress;
+    }
+
+    public void developCure(double amount) {
+        progress += amount;
+
+        if (progress > 100) {
+            progress = 100;
+        }
+    }
+
+    public boolean isComplete() {
+        return progress >= 100;
+    }
+
+    public double applyCure(Disease disease) {
+        if (!isComplete()) {
+            return disease.getInfectionRate();
+        }
+
+        return disease.getInfectionRate() * 0.5;
+    }
+
+    @Override
+    public String toString() {
+        return "Cure Progress: " + progress + "%";
+    }
 }
