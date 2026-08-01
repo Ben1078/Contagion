2f1eb1b42e0cbab7c4e8e74d7f529f2076d75970
yujie-chen119 <blairyujie@gmail.com>
2026-08-02 06:46:07 +0800

    Update Disease.java

--- src/contagion/Disease.java
+++ src/contagion/Disease.java
@@ -1,5 +1,46 @@
-package contagion;
+package Contagion;
 
-public class Disease {
+/**
+ * Stores information about a disease.
+ *
+ * @author YU-JIE CHEN
+ */
+public class disease {
 
+    private String name;
+    private double infectionRate;
+    private double recoveryRate;
+
+    public disease(String name, double infectionRate, double recoveryRate) {
+        this.name = name;
+        this.infectionRate = infectionRate;
+        this.recoveryRate = recoveryRate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getInfectionRate() {
+        return infectionRate;
+    }
+
+    public double getRecoveryRate() {
+        return recoveryRate;
+    }
+
+    public void setInfectionRate(double infectionRate) {
+        this.infectionRate = infectionRate;
+    }
+
+    public void setRecoveryRate(double recoveryRate) {
+        this.recoveryRate = recoveryRate;
+    }
+
+    @Override
+    public String toString() {
+        return "Disease: " + name
+                + ", Infection Rate: " + infectionRate
+                + ", Recovery Rate: " + recoveryRate;
+    }
 }
