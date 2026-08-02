19ed0d65448ab6bae3c3cdc1c0186b0d03d7b830
yujie-chen119 <blairyujie@gmail.com>
2026-08-02 10:31:18 +0800

    Update Country.java

--- src/contagion/Country.java
+++ src/contagion/Country.java
@@ -3,6 +3,38 @@ package contagion;
 /**
  * Represents a country in the pandemic network.
  *
+ * @author YU-JIE CHEN
  */
 public class Country {
+
+    private String name;
+    private int population;
+    private boolean infected;
+
+    public Country(String name, int population) {
+        this.name = name;
+        this.population = population;
+        this.infected = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public boolean isInfected() {
+        return infected;
+    }
+
+    public void setInfected(boolean infected) {
+        this.infected = infected;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (Population: " + population + ")";
+    }
 }
