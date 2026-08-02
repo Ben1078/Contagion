f0f3c867c7e6dd13f5eae8c37a30dbf54cd91208
yujie-chen119 <blairyujie@gmail.com>
2026-08-02 11:13:31 +0800

    Update TravelRoute.java

--- src/contagion/TravelRoute.java
+++ src/contagion/TravelRoute.java
@@ -3,6 +3,36 @@ package contagion;
 /**
  * Represents a travel route between two countries.
  *
+ * @author YU-JIE CHEN
  */
 public class TravelRoute {
+
+    private Country startCountry;
+    private Country endCountry;
+    private int distance;
+
+    public TravelRoute(Country startCountry, Country endCountry, int distance) {
+        this.startCountry = startCountry;
+        this.endCountry = endCountry;
+        this.distance = distance;
+    }
+
+    public Country getStartCountry() {
+        return startCountry;
+    }
+
+    public Country getEndCountry() {
+        return endCountry;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    @Override
+    public String toString() {
+        return startCountry.getName() + " -> "
+                + endCountry.getName()
+                + " (" + distance + " km)";
+    }
 }
